020050f24c24eff884b0cf545281f23f000bdb3e
Artur Zakirov <a.zakirov@postgrespro.ru>
2016-01-22 17:07:00 +0300

    Modified documentation for dictionary modules

--- contrib/hunspell_fr/sql/hunspell_fr.sql
+++ contrib/hunspell_fr/sql/hunspell_fr.sql
@@ -1,16 +1,20 @@
 CREATE EXTENSION hunspell_fr;
 
 CREATE TABLE table1(name varchar);
-INSERT INTO table1 VALUES ('beau'), ('antérieur'), ('fraternel'),
-						('plaît'), ('comprends'), ('désolée'),
-						('cents'), ('grammairiens'), ('résistèrent'),
-						('derniers'), ('rapprochent');
+INSERT INTO table1 VALUES ('batifoler'), ('batifolant'), ('batifole'), ('batifolait'),
+						('consentant'), ('consentir'), ('consentiriez');
 
-SELECT ts_lexize('public.french_hunspell', name) FROM table1;
+SELECT d.* FROM table1 AS t, LATERAL ts_debug('public.french', t.name) AS d;
 
 CREATE INDEX name_idx ON table1 USING GIN (to_tsvector('public.french', "name"));
-SELECT d.* FROM table1 AS t, LATERAL ts_debug('public.french', t.name) AS d;
+SELECT * FROM table1 WHERE to_tsvector('public.french', name)
+	@@ to_tsquery('public.french', 'batifolant');
+SELECT * FROM table1 WHERE to_tsvector('public.french', name)
+	@@ to_tsquery('public.french', 'consentiriez');
 
 DROP INDEX name_idx;
 CREATE INDEX name_idx ON table1 USING GIST (to_tsvector('public.french', "name"));
-SELECT d.* FROM table1 AS t, LATERAL ts_debug('public.french', t.name) AS d;
+SELECT * FROM table1 WHERE to_tsvector('public.french', name)
+	@@ to_tsquery('public.french', 'batifolant');
+SELECT * FROM table1 WHERE to_tsvector('public.french', name)
+	@@ to_tsquery('public.french', 'consentiriez');
--- contrib/hunspell_ru_ru/sql/hunspell_ru_ru.sql
+++ contrib/hunspell_ru_ru/sql/hunspell_ru_ru.sql
@@ -1,15 +1,25 @@
 CREATE EXTENSION hunspell_ru_ru;
 
 CREATE TABLE table1(name varchar);
-INSERT INTO table1 VALUES ('распространённости'), ('межнационального'),
-						('включается'), ('значу'), ('зевнув'),
-						('зевоты'), ('землей'), ('волчатами'), ('мойтесь');
+INSERT INTO table1 VALUES ('земля'), ('землей'), ('землями'), ('земли'),
+						('туши'), ('тушь'), ('туша'), ('тушат'),
+						('тушью');
 
-SELECT ts_lexize('public.russian_hunspell', name) FROM table1;
+SELECT d.* FROM table1 AS t, LATERAL ts_debug('public.russian', t.name) AS d;
 
 CREATE INDEX name_idx ON table1 USING GIN (to_tsvector('public.russian', "name"));
-SELECT d.* FROM table1 AS t, LATERAL ts_debug('public.russian', t.name) AS d;
+SELECT * FROM table1 WHERE to_tsvector('public.russian', name)
+	@@ to_tsquery('public.russian', 'землей');
+SELECT * FROM table1 WHERE to_tsvector('public.russian', name)
+	@@ to_tsquery('public.russian', 'тушь');
+SELECT * FROM table1 WHERE to_tsvector('public.russian', name)
+	@@ to_tsquery('public.russian', 'туша');
 
 DROP INDEX name_idx;
 CREATE INDEX name_idx ON table1 USING GIST (to_tsvector('public.russian', "name"));
-SELECT d.* FROM table1 AS t, LATERAL ts_debug('public.russian', t.name) AS d;
+SELECT * FROM table1 WHERE to_tsvector('public.russian', name)
+	@@ to_tsquery('public.russian', 'землей');
+SELECT * FROM table1 WHERE to_tsvector('public.russian', name)
+	@@ to_tsquery('public.russian', 'тушь');
+SELECT * FROM table1 WHERE to_tsvector('public.russian', name)
+	@@ to_tsquery('public.russian', 'туша');
