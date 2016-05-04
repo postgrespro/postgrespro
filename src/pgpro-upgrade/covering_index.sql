c6d96b05c9ed429ac8304048d14f1a84cac2a34c
Anastasia <a.lubennikova@postgrespro.ru>
2016-05-04 18:20:24 +0300

    update pg_class

--- src/pgpro-upgrade/covering_index.sql
+++ src/pgpro-upgrade/covering_index.sql
@@ -3,3 +3,4 @@ select
 attrelid, 'conincluding', atttypid, attstattarget, attlen, 25, attndims, attcacheoff, atttypmod, attbyval, attstorage, attalign, attnotnull, atthasdef, attisdropped, attislocal, attinhcount, attcollation, attacl, attoptions, attfdwoptions
 from pg_attribute 
 where attrelid = 'pg_constraint'::regclass::oid and attname ='conkey';
+update pg_class set relnatts=25 where relname='pg_constraint';
