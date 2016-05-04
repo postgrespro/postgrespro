3a0ccc07da4c41d69af2a172800fdc058d85dcbf
Alexander Korotkov <a.korotkov@postgrespro.ru>
2016-05-04 12:58:57 +0300

    Fix migration for pgpro_version(): use pg_catalog schema.

--- src/pgpro-upgrade/pgpro_version.sql
+++ src/pgpro-upgrade/pgpro_version.sql
@@ -1,1 +1,1 @@
-CREATE FUNCTION pgpro_version() RETURNS TEXT AS 'pgpro_version' LANGUAGE internal STRICT IMMUTABLE;
+CREATE FUNCTION pg_catalog.pgpro_version() RETURNS TEXT AS 'pgpro_version' LANGUAGE internal STRICT IMMUTABLE;
