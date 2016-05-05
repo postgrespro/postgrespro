99a831fb62fc46b175221c55af8f3f62632acc3e
Anastasia <a.lubennikova@postgrespro.ru>
2016-05-05 10:38:51 +0300

    covering_index scripts are fixed

--- src/pgpro-upgrade/covering_index.sql
+++ src/pgpro-upgrade/covering_index.sql
@@ -1,6 +1,2 @@
-insert into pg_attribute
-select 
-attrelid, 'conincluding', atttypid, attstattarget, attlen, 25, attndims, attcacheoff, atttypmod, attbyval, attstorage, attalign, attnotnull, atthasdef, attisdropped, attislocal, attinhcount, attcollation, attacl, attoptions, attfdwoptions
-from pg_attribute 
-where attrelid = 'pg_constraint'::regclass::oid and attname ='conkey';
+insert into pg_attribute select attrelid, 'conincluding', atttypid, attstattarget, attlen, 25, attndims, attcacheoff, atttypmod, attbyval, attstorage, attalign, attnotnull, atthasdef, attisdropped, attislocal, attinhcount, attcollation, attacl, attoptions, attfdwoptions from pg_attribute where attrelid = 'pg_constraint'::regclass::oid and attname ='conkey';
 update pg_class set relnatts=25 where relname='pg_constraint';
