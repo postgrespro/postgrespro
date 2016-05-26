4ee2ad87d49a222f1781ecd66a231a7c930c3833
stalkerg <stalkerg@gmail.com>
2016-05-26 18:42:23 +0300

    Rename migration scripts and fix small regression test.

---
+++ src/pgpro-upgrade/006-ptrack_test.sql
@@ -0,0 +1,1 @@
+CREATE FUNCTION pg_catalog.pg_ptrack_test(oid) RETURNS _int4 AS 'pg_ptrack_test' LANGUAGE internal STRICT VOLATILE;
--- src/pgpro-upgrade/ptrack_test.sql
+++
@@ -1,1 +0,0 @@
-CREATE FUNCTION pg_catalog.pg_ptrack_test(oid) RETURNS anyarray AS 'pg_ptrack_test' LANGUAGE internal STRICT VOLATILE;
