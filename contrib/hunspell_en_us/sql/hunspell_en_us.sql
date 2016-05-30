1d14ead90c2a6923f3561150cd6b3d2c436a071f
Artur Zakirov <a.zakirov@postgrespro.ru>
2016-05-30 19:24:25 +0300

    Remove hard coded public location

--- contrib/hunspell_en_us/sql/hunspell_en_us.sql
+++ contrib/hunspell_en_us/sql/hunspell_en_us.sql
@@ -1,24 +1,12 @@
 CREATE EXTENSION hunspell_en_us;
 
-CREATE TABLE table1(name varchar);
-INSERT INTO table1 VALUES ('leaves'), ('leaved'), ('leaving'),
-						('inability'), ('abilities'), ('disability'), ('ability');
-
-SELECT d.* FROM table1 AS t, LATERAL ts_debug('public.english', t.name) AS d;
-
-CREATE INDEX name_idx ON table1 USING GIN (to_tsvector('public.english', "name"));
-SELECT * FROM table1 WHERE to_tsvector('public.english', name)
-	@@ to_tsquery('public.english', 'leaving');
-SELECT * FROM table1 WHERE to_tsvector('public.english', name)
-	@@ to_tsquery('public.english', 'abilities');
-SELECT * FROM table1 WHERE to_tsvector('public.english', name)
-	@@ to_tsquery('public.english', 'ability');
-
-DROP INDEX name_idx;
-CREATE INDEX name_idx ON table1 USING GIST (to_tsvector('public.english', "name"));
-SELECT * FROM table1 WHERE to_tsvector('public.english', name)
-	@@ to_tsquery('public.english', 'leaving');
-SELECT * FROM table1 WHERE to_tsvector('public.english', name)
-	@@ to_tsquery('public.english', 'abilities');
-SELECT * FROM table1 WHERE to_tsvector('public.english', name)
-	@@ to_tsquery('public.english', 'ability');
+SELECT ts_lexize('english_hunspell', 'stories');
+SELECT ts_lexize('english_hunspell', 'traveled');
+SELECT ts_lexize('english_hunspell', 'eaten');
+SELECT ts_lexize('english_hunspell', 'I''m');
+SELECT ts_lexize('english_hunspell', 'Saturdays');
+SELECT ts_lexize('english_hunspell', 'healthcare');
+SELECT ts_lexize('english_hunspell', 'generally');
+SELECT ts_lexize('english_hunspell', 'integrating');
+SELECT ts_lexize('english_hunspell', 'lankiness''s');
+SELECT ts_lexize('english_hunspell', 'rewritten');
--- contrib/hunspell_nl_nl/sql/hunspell_nl_nl.sql
+++ contrib/hunspell_nl_nl/sql/hunspell_nl_nl.sql
@@ -1,20 +1,7 @@
 CREATE EXTENSION hunspell_nl_nl;
 
-CREATE TABLE table1(name varchar);
-INSERT INTO table1 VALUES ('deuren'), ('deurtje'), ('deur'),
-						('twee'), ('tweehonderd'), ('tweeduizend');
-
-SELECT d.* FROM table1 AS t, LATERAL ts_debug('public.dutch', t.name) AS d;
-
-CREATE INDEX name_idx ON table1 USING GIN (to_tsvector('public.dutch', "name"));
-SELECT * FROM table1 WHERE to_tsvector('public.dutch', name)
-	@@ to_tsquery('public.dutch', 'deurtje');
-SELECT * FROM table1 WHERE to_tsvector('public.dutch', name)
-	@@ to_tsquery('public.dutch', 'twee');
-
-DROP INDEX name_idx;
-CREATE INDEX name_idx ON table1 USING GIST (to_tsvector('public.dutch', "name"));
-SELECT * FROM table1 WHERE to_tsvector('public.dutch', name)
-	@@ to_tsquery('public.dutch', 'deurtje');
-SELECT * FROM table1 WHERE to_tsvector('public.dutch', name)
-	@@ to_tsquery('public.dutch', 'twee');
+SELECT ts_lexize('dutch_hunspell', 'speelde');
+SELECT ts_lexize('dutch_hunspell', 'duivels');
+SELECT ts_lexize('dutch_hunspell', 'huizes');
+SELECT ts_lexize('dutch_hunspell', 'vertelde');
+SELECT ts_lexize('dutch_hunspell', 'kunt');
