5a24efd5b3c05f61ff4553a06ed3d53920b447c9
Andres Freund <andres@anarazel.de>
2016-05-30 15:35:53 -0700

    Move memory barrier in UnlockBufHdr to before releasing the lock.
    This bug appears to have been introduced late in the development of
    48354581a4 ("Allow Pin/UnpinBuffer to operate in a lockfree
    manner.").
    
    Found while debugging a bug which turned out to be independent of the
    commit mentioned above.
    
    Backpatch: -

--- src/include/storage/buf_internals.h
+++ src/include/storage/buf_internals.h
@@ -230,8 +230,8 @@ typedef union BufferDescPadded
 extern uint32 LockBufHdr(BufferDesc *desc);
 #define UnlockBufHdr(desc, s)	\
 	do {	\
-		pg_atomic_write_u32(&(desc)->state, (s) & (~BM_LOCKED)); \
 		pg_write_barrier(); \
+		pg_atomic_write_u32(&(desc)->state, (s) & (~BM_LOCKED)); \
 	} while (0)
 
 
