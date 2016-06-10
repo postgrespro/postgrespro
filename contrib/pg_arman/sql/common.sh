bf99c52dcb2cffaa241a1bc02ccdea8a1030eb98
stalkerg <stalkerg@gmail.com>
2016-06-10 18:20:57 +0300

    Fix solaris tests again

--- contrib/pg_arman/sql/common.sh
+++ contrib/pg_arman/sql/common.sh
@@ -92,7 +92,7 @@ EOF
 
 function get_time_last_backup()
 {
-	set name_os = `uname`
+	name_os=`uname`
 	if [ "$name_os" == "SunOS" ]
 	then
 		pg_arman -B ${BACKUP_PATH} show | gtail -n +4 | head -n 1 | awk '{print($1, $2)}'
