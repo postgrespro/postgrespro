a4dbe4080b102056fcbf3f4321980fb2514baef5
Nikita Glukhov <n.gluhov@postgrespro.ru>
2016-11-12 16:53:08 +0300

    Use JsonValueWrapInBinary()

--- src/backend/utils/adt/json_generic.c
+++ src/backend/utils/adt/json_generic.c
@@ -397,9 +397,7 @@ jsonvArrayIteratorNext(JsonIterator **it, JsonValue *res, bool skipNested)
 		else if (res->type != jbvBinary)
 		{
 			Assert(res->type == jbvArray || res->type == jbvObject);
-			res->val.binary.data = JsonValueToContainer(val);
-			res->val.binary.uniquified = JsonValueIsUniquified(val);
-			res->type = jbvBinary;
+			JsonValueWrapInBinary(val, res);
 		}
 	}
 
@@ -449,10 +447,7 @@ jsonvObjectIteratorNext(JsonIterator **it, JsonValue *res, bool skipNested)
 			else if (res->type != jbvBinary)
 			{
 				Assert(res->type == jbvArray || res->type == jbvObject);
-				res->val.binary.data = JsonValueToContainer(&pair->value);
-				res->val.binary.uniquified =
-											JsonValueIsUniquified(&pair->value);
-				res->type = jbvBinary;
+				JsonValueWrapInBinary(&pair->value, res);
 			}
 		}
 
