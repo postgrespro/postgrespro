glukhovn/postgres

Remove JsonValue.val.binary.len field

src/backend/utils/adt/jsonfuncs.c
@@ -2439,7 +2439,8 @@ populate_record_worker(FunctionCallInfo fcinfo, const char *funcname,
 				else if (v->type == jbvObject || v->type == jbvArray)
 					s = JsonbToCString(NULL, JsonValueToContainer(v), 0);
 				else if (v->type == jbvBinary)
-					s = JsonbToCString(NULL, (JsonbContainer *) v->val.binary.data, v->val.binary.len);
+					s = JsonbToCString(NULL, v->val.binary.data,
+									   v->val.binary.data->len);
 				else
 					elog(ERROR, "unrecognized jsonb type: %d", (int) v->type);
 			}
@@ -2740,7 +2741,8 @@ make_row_from_rec_and_jsonb(Jsonb *element, PopulateRecordsetState *state)
 			else if (v->type == jbvObject || v->type == jbvArray)
 				s = JsonbToCString(NULL, JsonValueToContainer(v), 0);
 			else if (v->type == jbvBinary)
-				s = JsonbToCString(NULL, (JsonbContainer *) v->val.binary.data, v->val.binary.len);
+				s = JsonbToCString(NULL, v->val.binary.data,
+								   v->val.binary.data->len);
 			else
 				elog(ERROR, "unrecognized jsonb type: %d", (int) v->type);
 

postgrespro/postgrespro

src/backend/utils/adt/jsonfuncs.c
@@ -2911,7 +2911,7 @@ populate_scalar(ScalarIOData *io, Oid typid, int32 typmod, JsValue *jsv)
 			str = JsonbToCString(NULL, JsonValueToContainer(jbv), 0);
 		else if (jbv->type == jbvBinary)
 			str = JsonbToCString(NULL, jbv->val.binary.data,
-									   jbv->val.binary.len);
+									   jbv->val.binary.data->len);
 		else
 			elog(ERROR, "unrecognized jsonb type: %d", (int) jbv->type);
 	}
@@ -3335,7 +3335,7 @@ populate_record_worker(FunctionCallInfo fcinfo, const char *funcname,
 		/* fill binary jsonb value pointing to jb */
 		jbv.type = jbvBinary;
 		jbv.val.binary.data = &jb->root;
-		jbv.val.binary.len = jb->root.len;
+		jbv.val.binary.uniquified = true;
 	}
 
 	rettuple = populate_composite(&cache->io, tupType, tupTypmod,